85faa17b2574b7e90c3a2578b4bf7db4700eb39f
RamanSlinka <romuch1986@gmail.com>
2022-04-23 14:38:26 +0200

    added auth router, auth middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,6 +5,7 @@ const config = require('config')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {check, validationResult} = require('express-validator');
+const authMiddleware = require('../middleware/auth.middleware')
 
 router.post('/registration',
 
@@ -73,4 +74,27 @@ router.post('/login',
         }
     })
 
+
+router.get('/auth', authMiddleware,
+    async (req, res) => {
+        try {
+const user = await User.findOne({_id: req.user.id})
+
+            const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: '1h'})
+
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            res.send({message: 'Server error'})
+        }
+    })
 module.exports = router
