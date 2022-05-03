8e008a2011efe404f62f48aa5c5c055d21852d5d
RamanSlinka <romuch1986@gmail.com>
2022-05-03 22:41:11 +0200

    Added File sorting

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -3,6 +3,7 @@ const User = require('../models/User')
 const File = require('../models/File')
 const config = require("config");
 const fs = require('fs');
+const {query} = require("express-validator");
 
 
 class FileController {
@@ -30,7 +31,24 @@ class FileController {
 
     async getFiles(req, res) {
         try {
-            const files = await File.find({user: req.user.id, parent: req.query.parent})
+            const {sort} = req.query
+            let files
+            switch (sort) {
+                case 'name':
+                    files = await File.find({user: req.user.id, parent: req.query.parent}).sort({name:1})
+                    break
+                case 'type':
+                    files = await File.find({user: req.user.id, parent: req.query.parent}).sort({type:1})
+                    break
+                case 'date':
+                    files = await File.find({user: req.user.id, parent: req.query.parent}).sort({date:1})
+                    break
+                default:
+                    files = await File.find({user: req.user.id, parent: req.query.parent})
+                    break;
+            }
+
+
             return res.json(files)
         } catch (e) {
             console.log(e)
