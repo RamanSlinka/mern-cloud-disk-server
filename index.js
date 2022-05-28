abcec91b35c602721eb97551c36633d1f228f35b
RamanSlinka <romuch1986@gmail.com>
2022-05-28 15:35:54 +0200

    fixed bug (CORS)

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const config = require('config');
 const authRouter = require('./routes/auth.routes');
 const fileUpLoad = require('express-fileupload');
 const fileRouter = require('./routes/file.routes');
-const corsMiddleware = require('./middleware/cors.middleware');
+// const corsMiddleware = require('./middleware/cors.middleware');
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || config.get('serverPort')
 
 app.use(fileUpLoad({}))
-app.use(corsMiddleware)
+app.use(cors())
 app.use(express.json())
 app.use(express.static('static'))
 app.use('/api/auth', authRouter)
@@ -22,7 +23,6 @@ const start = async () => {
 
     try {
         await mongoose.connect(config.get('dbUrl'))
-
         app.listen(PORT, () => {
             console.log(`Server started on ${PORT} PORT` )
         })
