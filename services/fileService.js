599d7c3496d5920ae5a86cbc2a59fd7eca7fcb85
RamanSlinka <romuch1986@gmail.com>
2022-06-09 15:31:30 +0200

    some changes for deploying on Heroku

--- services/fileService.js
+++ services/fileService.js
@@ -4,8 +4,8 @@ const config = require('config')
 
 class FileService {
 
-    createDir(file) {
-        const filePath = `${config.get('filePath')}\\${file.user}\\${file.path}`
+    createDir(req, file) {
+        const filePath = this.getPath(req, file)
         return new Promise(((resolve, reject) => {
             try {
                 if (!fs.existsSync(filePath)) {
@@ -20,8 +20,8 @@ class FileService {
         }))
     }
 
-    deleteFile(file) {
-        const path = this.getPath(file)
+    deleteFile(req, file) {
+        const path = this.getPath(req, file)
         if (file.type === 'dir') {
             fs.rmdirSync(path)
         } else {
@@ -29,8 +29,8 @@ class FileService {
         }
     }
 
-    getPath(file) {
-        return config.get('filePath') + '\\' + file.user + '\\' + file.path
+    getPath(req, file) {
+        return req.filePath + '\\' + file.user + '\\' + file.path
     }
  }
 
